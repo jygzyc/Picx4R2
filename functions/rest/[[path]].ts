3a45d663904ca0d06c39629a29a8e63e23b5a267
ecool <jyg.zyc@outlook.com>
2024-06-16 02:50:36 +0800

    test

--- functions/rest/[[path]].ts
+++ functions/rest/[[path]].ts
@@ -1,4 +1,4 @@
-import { EventContext, PagesFunction, R2Bucket } from '@cloudflare/workers-types';
+import { PagesFunction, R2Bucket } from '@cloudflare/workers-types';
 import { error } from 'itty-router-extras';
 
 export interface Env {
@@ -12,11 +12,11 @@ export const onRequest: PagesFunction<Env> = async (context) => {
         async (module) => (await import('./routes'), module)
     );
 
-    try {
+    // try {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const response: Response | undefined = await router.handle(context.request, context.env);
         return response ?? error(404, 'not found');
-    } catch (err) {
-        return error(500, (err as Error).message);
-    }
+    // } catch (err) {
+    //     return error(500, (err as Error).message);
+    // }
 };
